a1ac0bd57115a41549d1028a3620b5842b64cc2a
termdev <deschaffel97@gmail.com>
2025-11-16 11:45:37 -0500

    update install of pacman packages

--- installs/arch.sh
+++ installs/arch.sh
@@ -1,7 +1,14 @@
-PAC_PACKAGES="hyprland kitty waybar dunst polkit-kde-agent qt5-wayland qt6-wayland xdg-desktop-portal-hyprland pipewire pipewire-pulse wireplumber grim slurp wl-clipboard wofi dolphin networkmanager nm-applet hyprlock sddm"
 AUR_PACKAGES=""
+PAC_PACKAGES=(
+    hyprland kitty waybar dunst polkit-kde-agent
+    qt5-wayland qt6-wayland xdg-desktop-portal-hyprland
+    pipewire pipewire-pulse wireplumber
+    grim slurp wl-clipboard wofi dolphin
+    networkmanager nm-applet hyprlock sddm cmake
+)
 
-sudo pacman -Syu --needed git base-devel --noconfirm $PACKAGES $PAC_PACKAGES
+
+sudo pacman -Syu --needed git base-devel --noconfirm $PACKAGES "${PAC_PACKAGES[@]}"
 
 sudo systemctl enable sddm
 sudo systemctl enable --now NetworkManager
