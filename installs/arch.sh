dcbfc9c53004f45c853d8f4e0b981e28be919d68
termdev <deschaffel97@gmail.com>
2025-11-16 13:29:29 -0500

    add yay and chrome as well as relading

--- installs/arch.sh
+++ installs/arch.sh
@@ -1,4 +1,4 @@
-AUR_PACKAGES=""
+AUR_PACKAGES="google-chrome"
 PAC_PACKAGES=(
     hyprland kitty waybar dunst polkit-kde-agent
     qt5-wayland qt6-wayland xdg-desktop-portal-hyprland
@@ -13,5 +13,14 @@ sudo pacman -Syu --needed git base-devel --noconfirm $PACKAGES "${PAC_PACKAGES[@
 sudo systemctl enable sddm
 sudo systemctl enable --now NetworkManager
 
+git clone https://aur.archlinux.org/yay.git
+cd yay
+
+# Build and install
+makepkg -si
+
+# Clean up (optional)
+cd ..
+rm -rf yay
 
 yay -Syu --noconfirm $AUR_PACKAGES
