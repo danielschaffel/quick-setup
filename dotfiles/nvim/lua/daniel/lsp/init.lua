717cb961e4835e26b6d2f0302813735daa4b6456
termdev <deschaffel97@gmail.com>
2025-11-18 15:03:34 -0500

    update: nvim lsp config

--- dotfiles/nvim/lua/daniel/lsp/init.lua
+++ dotfiles/nvim/lua/daniel/lsp/init.lua
@@ -1,49 +1,75 @@
 require("mason").setup()
 require("mason-lspconfig").setup()
 
-local status_ok, lspconfig = pcall(require, "lspconfig")
-if not status_ok then
-	return
+-- Common LSP configuration
+local function get_capabilities()
+	return require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 end
 
-local function config(_config)
-	return vim.tbl_deep_extend("force", {
-		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-		on_attach = function()
-			vim.keymap.set("n","gd", function() vim.lsp.buf.definition() end)
-			vim.keymap.set("n","K", function() vim.lsp.buf.hover() end)
-			vim.keymap.set("n","<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
-			vim.keymap.set("n","<leader>vd", function() vim.diagnostic.open_float() end)
-			vim.keymap.set("n","]d", function() vim.diagnostic.goto_next() end)
-			vim.keymap.set("n","[d", function() vim.diagnostic.goto_prev() end)
-			vim.keymap.set("n","<leader>ca", function() vim.lsp.buf.code_action() end)
-			vim.keymap.set("n","<leader>vco", function() vim.lsp.buf.code_action() end)
-			vim.keymap.set("n","<leader>vrr", function() vim.lsp.buf.references() end)
-			vim.keymap.set("n","<leader>lf", function() vim.lsp.buf.format() end)
-			vim.keymap.set("n","<leader>vrn", function() vim.lsp.buf.rename() end)
-			vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
-		end,
-	}, _config or {})
+local function on_attach()
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end)
+	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end)
+	vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end)
+	vim.keymap.set("n", "<leader>vco", function() vim.lsp.buf.code_action() end)
+	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end)
+	vim.keymap.set("n", "<leader>lf", function() vim.lsp.buf.format() end)
+	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end)
+	vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end)
 end
 
--- require("daniel.lsp.handlers").setup()
-require("lspconfig").pyright.setup(config(require("daniel.lsp.settings.pyright")))
--- require("lspconfig").lua_ls.setup(config(require("daniel.lsp.settings.sumneko_lua")))
-require("lspconfig").tsserver.setup(config())
--- require("lspconfig").svelte.setup(config())
--- require("lspconfig").dockerls.setup(config())
--- require("lspconfig").jsonls.setup(config())
--- require("lspconfig").html.setup(config())
--- require("lspconfig").cssls.setup(config())
--- require("lspconfig").rust_analyzer.setup(config())
--- require("lspconfig").bashls.setup(config())
-require("lspconfig").gopls.setup(config())
--- require("lspconfig").tailwindcss.setup(config())
-require("lspconfig").intelephense.setup(config())
-require("lspconfig").kotlin_language_server.setup(config())
-require("lspconfig").sqlls.setup(config())
--- require("lspconfig").cpptools.setup(config())
---
---
--- require "daniel.lsp.null-ls"
+-- Helper function to merge configs
+local function config(extra_config)
+	local base = {
+		capabilities = get_capabilities(),
+		on_attach = on_attach,
+	}
+	return vim.tbl_deep_extend("force", base, extra_config or {})
+end
 
+-- Configure LSP servers using vim.lsp.config (new API)
+vim.lsp.config.pyright = config(require("daniel.lsp.settings.pyright"))
+vim.lsp.config.ts_ls = config({})  -- Changed from tsserver to ts_ls
+vim.lsp.config.gopls = config({})
+vim.lsp.config.intelephense = config({})
+vim.lsp.config.kotlin_language_server = config({})
+vim.lsp.config.sqlls = config({})
+
+-- Enable the LSP servers
+vim.lsp.enable("pyright")
+vim.lsp.enable("ts_ls")
+vim.lsp.enable("gopls")
+vim.lsp.enable("intelephense")
+vim.lsp.enable("kotlin_language_server")
+vim.lsp.enable("sqlls")
+
+-- Commented out servers (uncomment and add vim.lsp.enable() as needed)
+-- vim.lsp.config.lua_ls = config(require("daniel.lsp.settings.sumneko_lua"))
+-- vim.lsp.enable("lua_ls")
+
+-- vim.lsp.config.svelte = config({})
+-- vim.lsp.enable("svelte")
+
+-- vim.lsp.config.dockerls = config({})
+-- vim.lsp.enable("dockerls")
+
+-- vim.lsp.config.jsonls = config({})
+-- vim.lsp.enable("jsonls")
+
+-- vim.lsp.config.html = config({})
+-- vim.lsp.enable("html")
+
+-- vim.lsp.config.cssls = config({})
+-- vim.lsp.enable("cssls")
+
+-- vim.lsp.config.rust_analyzer = config({})
+-- vim.lsp.enable("rust_analyzer")
+
+-- vim.lsp.config.bashls = config({})
+-- vim.lsp.enable("bashls")
+
+-- vim.lsp.config.tailwindcss = config({})
+-- vim.lsp.enable("tailwindcss")
